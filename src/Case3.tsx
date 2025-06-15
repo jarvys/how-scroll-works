17caf4e6bc581e0c610dc8534d7155350c10ec89
杨辰 <leguroky@gmail.com>
2025-06-15 22:56:54 +0800

    refine

--- src/Case3.tsx
+++ src/Case3.tsx
@@ -32,6 +32,7 @@ export default function Case3() {
     function listener() {
       window.performance.mark("c3-scroll");
       console.log("c3-scroll");
+      alert("hello world");
     }
 
     scrollContainerRef.current?.addEventListener("scroll", listener);
